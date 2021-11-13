2d4a0aab60e27120179dced6faa50e9896007b2a
Valentyn98 <valentyn.postolenko98@gmail.com>
2021-11-13 23:29:50 +0200

    hw/cw 13/11/21

--- hw2/js.js
+++ hw2/js.js
@@ -1,19 +1,19 @@
-//2 - Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
+//1 - Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
 // (в першу, другу, третю или четверту частину години).
-// let time = +prompt('від 0 до 59');
-// if (time >= 0 && time <= 15){
-//     console.log('first');
-// }else if(time > 15 && time < 30){
-//     console.log('second');
-// }else if(time>= 30 && time <= 45){
-//     console.log('thirt');
-// }else if(time >45 && time <=59){
-//     console.log('foutrh');
+let time = +prompt('від 0 до 59');
+if (time >= 0 && time <= 15){
+    console.log('first');
+}else if(time > 15 && time < 30){
+    console.log('second');
+}else if(time>= 30 && time <= 45){
+    console.log('thirt');
+}else if(time >45 && time <=59){
+    console.log('foutrh');
 // }
 // ******************************************
 //2 - У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
-// let day = prompt(' від 1 до 31');
-// day >= 1 && day <=10 ? console.log('first') : day >= 11 && day <=20 ? console.log('second') :day >= 11 && day <=31 ? console.log('thirt') : 'none';
+let day = prompt(' від 1 до 31');
+day >= 1 && day <=10 ? console.log('first') : day >= 11 && day <=20 ? console.log('second') :day >= 11 && day <=31 ? console.log('thirt') : 'none';
 // ******************************************
 //3 - У нас є змінна test, якщо  не дорівнює true, виведіть 'Вірно', інакше виведіть 'Неправильно'.
 //     Перевірте роботу скрипта при test, що дорівнює true, false. Напишіть два варіанти скрипта - з коротким записом(тернаркою)
