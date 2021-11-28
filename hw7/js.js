976550d1018fbf438fee6920648ff5f46d7e6e50
Valentyn98 <valentyn.postolenko98@gmail.com>
2021-11-28 19:22:54 +0200

    rs

--- hw7/js.js
+++ hw7/js.js
@@ -227,3 +227,104 @@ function Prince (name, years, tyflya) {
  }
 loveordie (popelisk, blackp)
 
+let prinsessi = popelisk.find((item)=>item.foot === blackp.tyflya);
+console.log(prinsessi)
+    // ***********************************************************AD********AD************ADDD***********************************************************
+// ***********************************************************AD********AD************ADDD***********************************************************
+// ***********************************************************AD********AD************ADDD***********************************************************
+// ***********************************************************AD********AD************ADDD***********************************************************
+// - Створити клас або функцію конструктор, за допомоги якої можна створювати об'єкти наступного вигляду.
+// Конструктор повинен приймати значення для кожної властивості, в т.ч і для властивостей внутрішніх об'єктів
+function Infous (id,name,username,email,adr ,phone,website) {
+    this.id = id;
+    this.name = name;
+    this.username = username;
+    this.email = email;
+    this.adr = adr;
+    this.phone = phone;
+    this.website = website;
+}
+    class Adr {
+        constructor(street, suite, city, zipcode, geo) {
+            this.street = street;
+            this.suite = suite;
+            this.city = city;
+            this.zipcode = zipcode;
+            this.geo = geo;
+        }
+    }
+        class Gg {
+            constructor(lat,lng) {
+                this.lat = lat;
+                this.lng = lng;
+            }
+        }
+        class Company {
+            constructor(name,catchPhrase,bs) {
+                this.name = name;
+                this.catchPhrase = catchPhrase;
+                this.bs = bs;
+            }
+        }
+
+let geog = new Gg ('-37.3159','81.1496')
+let adr = new Adr ('Kulas Light','Apt. 556','Gwenborough','92998-3874',geog)
+let comp = new Company ('Romaguera-Crona','Multi-layered client-server neural-net')
+let infouser = new Infous (1,'Leanne Graham','Bret','Sincere@april.biz',adr,'1-770-736-8031 x56442','hildegard.org',comp)
+
+console.log(infouser)
+// {
+//v     id: 1,
+//v         name: 'Leanne Graham',
+//v     username: 'Bret',
+//v     email: 'Sincere@april.biz',
+// v    address: {
+// v    street: 'Kulas Light',
+// v        suite: 'Apt. 556',
+//         city: 'Gwenborough',
+//         zipcode: '92998-3874',
+//         geo: {
+//         lat: '-37.3159',
+//             lng: '81.1496'
+//     }
+// },
+//     phone: '1-770-736-8031 x56442',
+//         website: 'hildegard.org',
+//     company: {
+//     name: 'Romaguera-Crona',
+//         catchPhrase: 'Multi-layered client-server neural-net',
+//         bs: 'harness real-time e-markets'
+// }
+// }
+
+// -  Створити функцію конструктор / клас  який описує об'єкт тегу
+// Поля :
+//     -назва тегу ()
+// - опис його дій
+// - масив з атрибутами (2-3 атрибути максимум)
+// Кожен атрибут описати як окремий який буде містити
+// -назву атрибуту
+// -опис дії атрибуту
+// інформацію брати з htmlbook.ru
+//
+// Таким чином описати теги
+// -a
+// -div
+// -h1
+// -span
+// -input
+// -form
+// -option
+// -select
+// Приклад результуючого об'єкту
+// {
+//     titleOfTag: 'area',
+//         action: `Каждый элемент <area> определяет активные области изображения, которые являются ссылками...`,
+//     attrs: [
+//     {titleOfAttr: 'accesskey', actionOfAttr: 'Переход к области с помощью комбинации клавиш'},
+//     {/*some props and values*/},
+//     {/*...*/},
+//     {/*...*/},
+// ]
+//
+// }
