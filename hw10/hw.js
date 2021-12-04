92684cf24b22db4b3688901f806b1727801e9682
Valentyn98 <valentyn.postolenko98@gmail.com>
2021-12-04 22:39:31 +0200

    hw

--- hw10/hw.js
+++ hw10/hw.js
@@ -13,27 +13,27 @@
 
 
 // 1- Создать произвольный елемент с id = text.  Используя JavaScript, сделайте так, чтобы при клике на кнопку исчезал элемент с id="text".
-let proizvol =  document.createElement('div')
-document.body.appendChild(proizvol)
-proizvol.innerText = 'Proizvol'
-
-let but = document.createElement('button')
-document.body.appendChild(but)
-but.innerText = 'BB'
-
-but.addEventListener('click', function (){
-    proizvol.style.display = 'none'
-})
-// 2- Создайте кнопку, при клике на которую, она будет скрывать сама себя.
-
-let butTwo = document.createElement('button')
-document.body.appendChild(butTwo)
-butTwo.innerText = 'Butbut'
-
-
-butTwo.addEventListener('click', function (){
-    butTwo.style.display = 'none'
-})
+// let proizvol =  document.createElement('div')
+// document.body.appendChild(proizvol)
+// proizvol.innerText = 'Proizvol'
+//
+// let but = document.createElement('button')
+// document.body.appendChild(but)
+// but.innerText = 'BB'
+//
+// but.addEventListener('click', function (){
+//     proizvol.style.display = 'none'
+// })
+// // 2- Создайте кнопку, при клике на которую, она будет скрывать сама себя.
+//
+// let butTwo = document.createElement('button')
+// document.body.appendChild(butTwo)
+// butTwo.innerText = 'Butbut'
+//
+//
+// butTwo.addEventListener('click', function (){
+//     butTwo.style.display = 'none'
+// })
 // 3- створити інпут який приймає вік людини та кнопку яка підтверджує дію.При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18, та повідомити про це користувача
 
 // let formss = document.createElement('form')
@@ -61,12 +61,12 @@ document.getElementById('button1').onclick = function (){
 }
 
 // -4 Создайте меню, которое раскрывается/сворачивается при клике
-let menu = document.getElementsByClassName('menumenu')[0]
-let butm = document.getElementById('but_menu')
-
-butm.addEventListener('click', function (){
-    menu.classList.toggle('menumenu');
-})
+// let menu = document.getElementsByClassName('menumenu')[0]
+// let butm = document.getElementById('but_menu')
+//
+// butm.addEventListener('click', function (){
+//     menu.classList.toggle('menumenu');
+// })
 
 // 5- Создать список комментариев , пример объекта коментария - {title : 'lorem', body:'lorem ipsum dolo sit ameti'}.-->
 //     Вывести список комментариев в документ, каждый в своем блоке.-->
@@ -79,15 +79,36 @@ let docs = [
     {title : 'Рубашка', body:'Футболка с рукавами'}
 ]
 let head = document.createElement('div')
-
+document.body.appendChild(head)
 
 for ( let blockdiv of docs ){
-     document.createElement('div')
-     document.createElement('button')
+     let divf = document.createElement('div')
+    head.appendChild(divf)
+    divf.classList = 'cld'
+    let pif = document.createElement('p')
+    divf.appendChild(pif)
+    let hp = document.createElement("h1")
+    divf.appendChild(hp)
+     let butf = document.createElement('button')
+    head.appendChild(butf)
+    butf.style.width = '40px'
+    butf.style.height = '20px'
+    butf.innerText = 'Hide'
+
+
+    hp.innerText = blockdiv.title
+    pif.innerText = blockdiv.body
+
+
+    butf.addEventListener('click',function (){
+        divf.classList.toggle('cld')
+        }
+
+    )
 }
 
 
-document.body.appendChild(head)
+
 
 
 
