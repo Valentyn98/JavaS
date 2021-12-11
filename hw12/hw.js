41422386e468be3711d3f049e5e785e498586f27
Valentyn98 <valentyn.postolenko98@gmail.com>
2021-12-11 19:32:49 +0200

    hw12

--- hw12/hw.js
+++ hw12/hw.js
@@ -5,36 +5,36 @@
 // Всі характеристики повинні мати свої блоки всередені div.post
 // https://jsonplaceholder.typicode.com/posts
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(function (response) {
-        let jsonB = response.json();
-        return jsonB;
-    }).then(users => {
-    let fathBlock = document.createElement('div')
-    document.body.appendChild(fathBlock)
-    fathBlock.classList.add('fatbl')
-
-            for (let us of users){
-                let mom = document.createElement('div')
-                fathBlock.appendChild(mom)
-                mom.classList.add('mom')
-                let divid = document.createElement('div')
-                mom.appendChild(divid)
-
-
-                let divtitle = document.createElement('div')
-                mom.appendChild(divtitle)
-
-                let divbody = document.createElement('div')
-                mom.appendChild(divbody)
-
-
-                divid.innerText = `ID: ${us.id}`
-                divtitle.innerText =  `Title: ${us.title}`
-                divbody.innerText = `Body: ${us.body}`
-
-            }
-    });
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(function (response) {
+//         let jsonB = response.json();
+//         return jsonB;
+//     }).then(users => {
+//     let fathBlock = document.createElement('div')
+//     document.body.appendChild(fathBlock)
+//     fathBlock.classList.add('fatbl')
+//
+//             for (let us of users){
+//                 let mom = document.createElement('div')
+//                 fathBlock.appendChild(mom)
+//                 mom.classList.add('mom')
+//                 let divid = document.createElement('div')
+//                 mom.appendChild(divid)
+//
+//
+//                 let divtitle = document.createElement('div')
+//                 mom.appendChild(divtitle)
+//
+//                 let divbody = document.createElement('div')
+//                 mom.appendChild(divbody)
+//
+//
+//                 divid.innerText = `ID: ${us.id}`
+//                 divtitle.innerText =  `Title: ${us.title}`
+//                 divbody.innerText = `Body: ${us.body}`
+//
+//             }
+//     });
 
 // Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті.
 //     Для кожного елементу свій блок div.comment
@@ -46,8 +46,46 @@ fetch('https://jsonplaceholder.typicode.com/comments')
     let xBet = res.json();
     return xBet;
 }).then(ous =>{
-
+let blComm = document.createElement('div')
+    document.body.appendChild(blComm)
+    blComm.classList.add('blComm')
     for (let ui of ous){
-        console.log(ui)
+
+        let korner = document.createElement('div')
+        korner.classList.add('kornik')
+        blComm.appendChild(korner)
+
+        let chPost  = document.createElement('div')
+        korner.appendChild(chPost)
+        chPost.classList.add('chPost')
+
+        let chId = document.createElement('div')
+        korner.appendChild(chId)
+        chId.classList.add('chId')
+
+        let chName = document.createElement('div')
+        korner.appendChild(chName)
+        chName.classList.add('chName')
+
+        let a = document.createElement('a')
+        a.setAttribute('href', '#');
+        korner.appendChild(a)
+        a.classList.add('a')
+
+        let chEmail = document.createElement('div')
+        a.appendChild(chEmail)
+        chEmail.classList.add('chEmail')
+
+        let chBody = document.createElement('div')
+        korner.appendChild(chBody)
+        chBody.classList.add('chBody')
+
+
+
+        chPost.innerText = `postID: ${ui.postId}`
+        chId.innerText = `id: ${ui.id}`
+        chName.innerText = `${ui.name}`
+        chEmail.innerText = `email: ${ui.email}`
+        chBody.innerText = `body: ${ui.body}`
     }
 })
