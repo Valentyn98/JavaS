1b3df399cc250afea62fe0135b81017bf52892d3
Valentyn98 <valentyn.postolenko98@gmail.com>
2021-12-17 22:36:56 +0200

    hw14 2var

--- hw14/hw2var.js
+++ hw14/hw2var.js
@@ -14,19 +14,6 @@ function sleep (sleep){
         },800)
     })
 }
-
-
-function glassOfWat (glass){
-    return new Promise((resolve, reject) => {
-        setTimeout(() =>{
-            if(glass){
-                resolve(heal += 10)
-            }
-        },1100)
-    })
-}
-
-
 function myNextDay(nextDayWell){
     return new Promise((resolve, reject) => {
         setTimeout(() =>{
@@ -39,6 +26,19 @@ function myNextDay(nextDayWell){
         },300)
     })
 }
+
+function glassOfWat (glass){
+    return new Promise((resolve, reject) => {
+        setTimeout(() =>{
+            if(glass){
+                resolve(heal += 10)
+            }
+        },1100)
+    })
+}
+
+
+
 let heal = 10
 
 function makeSport (mkSport){
@@ -48,18 +48,25 @@ function makeSport (mkSport){
                 resolve('make ')
                 console.log('make sport')
             }
-        },1100)
+        },100)
     })
 }
 sleep(true).then(well =>{
     console.log('spit ok')
-    return myNextDay(true)
+    return myNextDay(true)//так треба)
 
-})
-glassOfWat (true).then(asd =>{
+}).then(glass => {
     console.log('drink wat ')
-   return  makeSport (true)
+    return glassOfWat (true)
+}).then(mkSport =>{
+    return makeSport (mkSport)
 })
+
+
+// glassOfWat (true).then(asd =>{
+//     console.log('drink wat ')
+//    return  makeSport (true)
+// })
 
 
 
